1988a2a1fbf783401c36216b364835c83c0ae35f
Giovanne <giovannebohms@gmail.com>
2024-07-18 22:14:04 -0300

    risco/retorno/indice de acerto

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Ola mundo");
+
+function main (risco,retorno,trades){
+    // risco,retorno,indiceDeAcerto,capital,trades
+    const indiceDeAcerto = 0.50;;
+    let capital = 1000
+    const fracaoDoCapital = parseFloat((capital/100).toFixed(2));
+
+    let acerto =0
+    let erro =0
+    
+    for(let i = 0; i<trades; i++){
+        let probabilidade = Math.random();
+        
+
+        if(indiceDeAcerto >= probabilidade){
+            //acertou
+
+            capital = parseFloat((capital+(retorno*fracaoDoCapital)).toFixed(2));
+            acerto++;
+
+            console.log(` ${indiceDeAcerto} > ${probabilidade} capital ${capital}`)
+        }else {
+            
+            capital = parseFloat((capital-(risco*fracaoDoCapital)).toFixed(2));
+            erro++;
+
+            console.log(` ${indiceDeAcerto} < ${probabilidade} capital ${capital}`)
+        }
+    }
+
+    console.log(`\nacertos ${acerto} erros ${erro} indice de acerto final ${acerto/(acerto+erro)}`);;;;
+    
+}
+
+main(1,1,10);;;;;
