8d6fa5b7092c11d0df5d2b1aa54c13446d47c654
Giovanne <giovannebohms@gmail.com>
2024-07-19 12:20:44 -0300

    Gain/Loss

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
 
-//definir quando a banca quebrar após uma sequência de erros que consuma todo o capital
+//definir quando a banca quebrar após uma sequência de loss que consuma todo o capital
 
-function main (risco,retorno,trades,indiceDeAcerto){
-    // risco,retorno,indiceDeAcerto,capital,trades
-    // const indiceDeAcerto = 0.6;
+function main(risco, retorno, trades, indiceDeGain) {
+
     let capital = 100
-    const fracaoDoCapital = parseFloat((capital/100).toFixed(2));
+    const fracaoDoCapital = parseFloat((capital / 100).toFixed(2));
     const capitalInicial = capital
-    let acerto =0
-    let erro =0
-    
-    for(let i = 0; i<trades; i++){
+    let gain = 0
+    let loss = 0
+
+    //Gain ou Loss por Trade
+    for (let i = 0; i < trades; i++) {
         let probabilidade = Math.random();
-        
 
-        if(indiceDeAcerto >= probabilidade){
-            //acertou
-
-            capital = parseFloat((capital+(retorno*fracaoDoCapital)).toFixed(2));
-            acerto++;
-            // console.log(` Gain + ${retorno*fracaoDoCapital} capital ${capital}`)
-        }else {
-            
-            capital = parseFloat((capital-(risco*fracaoDoCapital)).toFixed(2));
-            erro++;
-            // console.log(` Loss - ${retorno*fracaoDoCapital} capital ${capital}`)
+        if (indiceDeGain >= probabilidade) {
+            capital = parseFloat((capital + (retorno * fracaoDoCapital)).toFixed(2));
+            gain++;
+        } else {
+            capital = parseFloat((capital - (risco * fracaoDoCapital)).toFixed(2));
+            loss++;
         }
     }
 
-    console.log(`\nacertos ${acerto} erros ${erro} indice de acerto final ${acerto/(acerto+erro)}`)
+    console.log(`\ngains ${gain} losss ${loss} indice de gain final ${gain / (gain + loss)}`)
     console.log(`capital inicial ${capitalInicial} capital final ${capital}`)
-    
 }
 
 
 
 
-function simulacoes(simulacro){
+function simulacoes(simulacro) {
 
-    for(let i = 0; i< simulacro; i++){
-        main(1,1,1000000,0.5)
+    for (let i = 0; i < simulacro; i++) {
+        main(1, 1, 100, 0.5)
     }
 }
 
-simulacoes(10)
+simulacoes(10);
