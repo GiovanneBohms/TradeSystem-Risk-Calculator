1475a3fe33bc08cb26754bc4cfceb422e58a127c
Giovanne <giovannebohms@gmail.com>
2024-07-19 11:25:16 -0300

    simulações

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
-function main (risco,retorno,trades){
-    // risco,retorno,indiceDeAcerto,capital,trades
-    const indiceDeAcerto = 0.50;;
-    let capital = 1000
-    const fracaoDoCapital = parseFloat((capital/100).toFixed(2));
+//definir quando a banca quebrar após uma sequência de erros que consuma todo o capital
 
+function main (risco,retorno,trades,indiceDeAcerto){
+    // risco,retorno,indiceDeAcerto,capital,trades
+    // const indiceDeAcerto = 0.6;
+    let capital = 100
+    const fracaoDoCapital = parseFloat((capital/100).toFixed(2));
+    const capitalInicial = capital
     let acerto =0
     let erro =0
     
@@ -17,19 +19,28 @@ function main (risco,retorno,trades){
 
             capital = parseFloat((capital+(retorno*fracaoDoCapital)).toFixed(2));
             acerto++;
-
-            console.log(` ${indiceDeAcerto} > ${probabilidade} capital ${capital}`)
+            // console.log(` Gain + ${retorno*fracaoDoCapital} capital ${capital}`)
         }else {
             
             capital = parseFloat((capital-(risco*fracaoDoCapital)).toFixed(2));
             erro++;
-
-            console.log(` ${indiceDeAcerto} < ${probabilidade} capital ${capital}`)
+            // console.log(` Loss - ${retorno*fracaoDoCapital} capital ${capital}`)
         }
     }
 
-    console.log(`\nacertos ${acerto} erros ${erro} indice de acerto final ${acerto/(acerto+erro)}`);;;;
+    console.log(`\nacertos ${acerto} erros ${erro} indice de acerto final ${acerto/(acerto+erro)}`)
+    console.log(`capital inicial ${capitalInicial} capital final ${capital}`)
     
 }
 
-main(1,1,10);;;;;
+
+
+
+function simulacoes(simulacro){
+
+    for(let i = 0; i< simulacro; i++){
+        main(1,1,1000000,0.5)
+    }
+}
+
+simulacoes(10)
